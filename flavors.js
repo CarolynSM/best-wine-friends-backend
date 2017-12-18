8fc4d65d8721eab2652da5a63d84d27b31bc53bd
Carolyn Selheim <carolynselheim@Carolyns-MacBook-Pro.local>
2017-12-18 11:24:45 -0700

    added flavor imgs

--- flavors.js
+++ flavors.js
@@ -3,82 +3,98 @@ var flavors = [
     flavorId: 1,
     ingredientId: 1,
     profile: "Earthy and Savory",
-    description: "root vegetables, mushrooms, lentils"
+    description: "root vegetables, mushrooms, lentils",
+    imgSrc: "assets/mushrooms.png"
   }, {
     flavorId: 2,
     ingredientId: 1,
     profile: "Light and Bright",
-    description: "fresh peas, celery, fresh greens, green beans"
+    description: "fresh peas, celery, fresh greens, green beans",
+    imgSrc: "assets/Citrus.png"
   }, {
     flavorId: 3,
     ingredientId: 1,
     profile: "Spicy",
-    description: "chili pepper and heat"
+    description: "chili pepper and heat",
+    imgSrc: "Spicy.png"
   }, {
     flavorId: 4,
     ingredientId: 1,
     profile: "Tangy",
-    description: "citrus and vinegar"
+    description: "citrus and vinegar",
+    imgSrc: "assets/Citrus.png"
   }, {
     flavorId: 5,
     ingredientId: 2,
     profile: "Rich",
-    description: "butter and cream"
+    description: "butter and cream",
+    imgSrc: "assets/butter.png"
   }, {
     flavorId: 6,
     ingredientId: 2,
     profile: "Fried",
-    description: "crispy and oily"
+    description: "crispy and oily",
+    imgSrc: "assets/Fried.png"
   }, {
     flavorId: 7,
     ingredientId: 2,
     profile: "Light and Bright",
-    description: "fresh herbs and citrus"
+    description: "fresh herbs and citrus",
+    imgSrc: "assets/Citrus.png"
   }, {
     flavorId: 8,
     ingredientId: 3,
     profile: "BBQ",
-    description: "sweet, spicy, and tangy"
+    description: "sweet, spicy, and tangy",
+    imgSrc: "assets/BBQ.png"
   }, {
     flavorId: 9,
     ingredientId: 3,
     profile: "Earthy",
-    description: "root vegetables, mushrooms, lentils"
+    description: "root vegetables, mushrooms, lentils",
+    imgSrc: "assets/mushrooms.png"
   }, {
     flavorId: 10,
     ingredientId: 3,
     profile: "Cheesy or Creamy",
-    description: "cream-based sauces"
+    description: "cream-based sauces",
+    imgSrc: "assets/Cheese.png"
   }, {
     flavorId: 11,
     ingredientId: 4,
     profile: "BBQ",
-    description: "sweet, spicy, and tangy"
+    description: "sweet, spicy, and tangy",
+    imgSrc: "assets/BBQ.png"
   }, {
     flavorId: 12,
     ingredientId: 4,
     profile: "Light and Earthy",
-    description: "herbs and apples"
+    description: "herbs and apples",
+    imgSrc: "assets/apple.png"
   }, {
     flavorId: 13,
     ingredientId: 4,
     profile: "Asian spiced",
-    description: "salty soy, spicy chili, and citrus sweet"
+    description: "salty soy, spicy chili, and citrus sweet",
+    imgSrc: "assets/Asian.png"
   }, {
     flavorId: 14,
     ingredientId: 5,
     profile: "Complex",
-    description: "cumin, harissa, chili, blue cheese..."
+    description: "cumin, harissa, chili, blue cheese...",
+    imgSrc: "assets/Complex.png"
   }, {
     flavorId: 15,
     ingredientId: 5,
     profile: "BBQ",
-    description: "sweet, spicy, and tangy"
+    description: "sweet, spicy, and tangy",
+    imgSrc: "assets/BBQ.png"
   }, {
     flavorId: 16,
     ingredientId: 5,
     profile: "Steak or Burger",
-    description: "classic and simple"
+    description: "classic and simple",
+    imgSrc: "assets/steak.png"
   }
 ];
 
