04dff79ea53338aea7772549921b7f4dd20ac99a
Carolyn Selheim <carolynselheim@Carolyns-MacBook-Pro.local>
2017-12-18 11:33:09 -0700

    added main imgs

--- food.js
+++ food.js
@@ -2,23 +2,28 @@ var food = [
   {
     id: 1,
     ingredient: "Vegetables",
-    flavors: [1, 2, 3, 4]
+    flavors: [1, 2, 3, 4],
+    imgSrc: "assets/Vegetables.png"
   }, {
     id: 2,
     ingredient: "Fish",
-    flavors: [1, 2, 3]
+    flavors: [1, 2, 3],
+    imgSrc: "assets/Fish.png"
   }, {
     id: 3,
     ingredient: "Poultry",
-    flavors: [1, 2, 3]
+    flavors: [1, 2, 3],
+    imgSrc: "assets/Poultry"
   }, {
     id: 4,
     ingredient: "Pork",
-    flavors: [1, 2, 3]
+    flavors: [1, 2, 3],
+    imgSrc: "assets/Pork.png"
   }, {
     id: 5,
     ingredient: "Beef",
-    flavors: [1, 2, 3]
+    flavors: [1, 2, 3],
+    imgSrc: "assets/Beef.png"
   }
 ];
 
